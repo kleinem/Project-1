87b10c69846fd562f9aa4c2fb6a75e90e85e1603
Klein <klein.misiurski@gmail.com>
2021-02-08 15:37:53 -0500

    basic scene created

---
+++ MGD1/MGD 1/Assets/Scripts/WedgeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WedgeController : MonoBehaviour
+{
+
+    public Color col;
+
+    void Start()
+    {
+
+        setColor(new Color(Random.Range(0, 1.0f), Random.Range(0, 1.0f), Random.Range(0, 1.0f)));
+
+    }
+
+    void Update()
+    {
+
+
+
+    }
+
+    public void setColor(Color col_)
+    {
+
+        col = col_;
+        transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().color = col_;
+
+    }
+
+    public void setSize(float size_)
+    {
+
+        transform.GetChild(0).localEulerAngles = new Vector3(0, 0, size_ - 90);
+        Debug.Log("setting to " + size_);
+
+    }
+
+}
---
+++ MGD1/MGD 1/Assets/Scripts/WheelController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelController : MonoBehaviour
+{
+
+    public GameObject wedge;
+
+    private Rigidbody2D rb;
+    private GameObject currentWedge;
+    private float lastSpawn = 180;
+    private bool spawning = false;
+
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+        updateSpeed(60);
+
+    }
+
+    void Update()
+    {
+
+        foreach (Touch t in Input.touches)
+        {
+
+            if (t.phase == TouchPhase.Ended)
+            {
+
+                setSpawning(!spawning);
+
+            }
+
+        }
+        if (spawning)
+        {
+
+            
+
+        }
+
+
+    }
+
+    public void setSpawning(bool spawning_)
+    {
+
+        if (spawning_)
+        {
+
+            lastSpawn = 180 - transform.eulerAngles.z;
+            spawning = true;
+
+        }
+        else
+        {
+
+            spawning = false;
+
+        }
+
+    }
+
+    private void spawnWedge(Color col_, float size_)
+    {
+
+        GameObject tmp = Instantiate(wedge, transform);
+        tmp.transform.localEulerAngles = new Vector3(0, 0, lastSpawn);
+        tmp.GetComponent<WedgeController>().setColor(col_);
+        tmp.GetComponent<WedgeController>().setSize(size_);
+        lastSpawn += size_;
+
+    }
+
+    public void updateSpeed(float speed_)
+    {
+
+        rb.angularVelocity = -speed_;
+
+    }
+
+}
